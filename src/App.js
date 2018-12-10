aa2bb8ee34cdf9ae05bf9861e760f7349e100917
SergioGutzB <sergut18@gmail.com>
2018-12-09 21:47:44 -0500

    create PayRoll Provider and imported on App, defined intial state on App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import employees from './employees';
+import PayrollProvider from './components/Payroll';
+
+const locale = {
+  MXM: 'MXM',
+  USD: 'USD'
+}
 
 class App extends Component {
+
+  state = {
+    employees,
+    dataSource: employees,
+    currency: locale.MXM,
+    mxm: 21.50,
+    handleToggleCurrency: (e) => {
+      const value = this.state.currency === locale.MXM ? locale.USD : locale.MXM;
+      this.setState({currency: value})
+    },
+  }
+
   render() {
     return (
       <div id='employees'>
+        <PayrollProvider {...this.state}>
+          <h1>Table</h1>
+        </PayrollProvider>
       </div>
     );
   }
---
+++ src/components/Payroll.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+const PayrollContext = React.createContext()
+
+export { PayrollContext }
+
+class PayrollProvider extends Component {
+  render () {
+    return (
+      <PayrollContext.Provider value={{ ...this.props }}>
+        {this.props.children}
+      </PayrollContext.Provider>
+    )
+  }
+}
+
+export default PayrollProvider
