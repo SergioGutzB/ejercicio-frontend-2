00a1e6dd3027ed6a42616ab972b7599048e3fc4f
Sergio Gutierrez <sgutierrez@portalfinance.co>
2018-12-10 17:53:01 -0500

    implemented function appliFilters to search employees by name and company

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,8 +4,22 @@ import { PayrollContext } from './Payroll';
 export default class SearchComponent extends Component {
 
   handleChange = (e, handle) => {
-    //e.preventDefault;
     console.log(e, handle);
+    const value = e.target.value;
+    const filter = {
+      name: value,
+      company: value
+    }
+    const dataSource = [...handle.employees];
+    console.log('filter employees:  ', this.applyFilters(dataSource, filter));
+  }
+
+  applyFilters = (data, filters) => {
+    return data.filter(item => Object.keys(filters)
+      .map(keyToFilterOn =>
+        item[keyToFilterOn].includes(filters[keyToFilterOn]),
+      ).reduce((x, y) => x || y)
+    );
   }
 
   render() {
