f824cbb1ff189f3bc92588782b91c29d77184b82
SergioGutzB <sergut18@gmail.com>
2018-12-10 21:33:02 -0500

    added button edit to after table whit function that change state edit

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,11 +4,25 @@ import { PayrollContext } from './Payroll';
 
 export default class TableComponent extends Component {
 
+  state = {
+    editabled: false,
+    adding: false
+  }
+
+  handleEdit = (evt, callback) => {
+    this.setState({editabled: !this.state.editabled})
+  }
+
   render() {
     return (
       <PayrollContext.Consumer>
         {(ctx) => {
-          return Object.keys(ctx.dataSource).map(key => <TableRow {...ctx.dataSource[key]}></TableRow>);
+          return (
+            <div >
+              {Object.keys(ctx.dataSource).map(key => <TableRow {...ctx.dataSource[key]} editabled={this.state.editabled} ></TableRow>) }
+              <button onClick={(e) => this.handleEdit(e, () => console.log('callback edit'))}>{!this.state.editabled ? 'Editar' : 'Guardar'}</button>
+            </div>
+          );
         }}
       </PayrollContext.Consumer>
     );
